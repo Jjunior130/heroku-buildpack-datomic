ec08e15ef4cbe07975c7a0ee996cb39a2506e4b1
Junior <Jjunior130@users.noreply.github.com>
2018-12-02 01:58:10 -0500

    Dev mode

--- lib/datomic-storage-config.sh
+++ lib/datomic-storage-config.sh
@@ -5,93 +5,27 @@
 
 OUTPUT_PROPERTIES_FILE=${SCRIPTS_TARGET_DIR}/transactor.properties
 
-configure_storage() {
-
-    STORAGE_TYPE=${DATOMIC_STORAGE_TYPE:-"HEROKU_POSTGRES"}
-
-    case ${STORAGE_TYPE} in
-
-        DYNAMODB)
-            configure_ddb
-            ;;
-
-        HEROKU_POSTGRES|POSTGRES)
-            configure_sql
-            ;;
-
-        *)  echo "Unsupported storage type '${STORAGE_TYPE}'" && return 1
-            ;;
-    esac
-}
-
-configure_ddb() {
-
-    echo -n "-----> Configuring Datomic to connect to DynamoDB... "
-
-    SAMPLE_PROPERTIES_FILE=${BUILD_DIR}/datomic/config/samples/ddb-transactor-template.properties
-    COPY_NAME=${PWD}/ddb-transactor-template-COPY.properties
-    PATCHED_FILE_NAME=${PWD}/ddb-transactor-template-PATCHED.properties
-
-    cp ${SAMPLE_PROPERTIES_FILE} ${COPY_NAME}
-
-    #TODO - put this chmod somewhere smarter
-    chmod -R +x ${BUILD_DIR}/datomic/bin
-
-    ${BUILD_DIR}/datomic/bin/datomic ensure-transactor ${COPY_NAME} ${PATCHED_FILE_NAME}
-
-    cat ${PATCHED_FILE_NAME} | license_filter > ${OUTPUT_PROPERTIES_FILE}
-
-    echo "done"
-}
-
 configure_sql() {
 
     [ -z "${DATABASE_URL}" ] && {
         echo "You must set DATABASE_URL" && return 1
     }
 
-    echo -n "-----> Configuring Datomic to connect to ${STORAGE_TYPE}... "
+    echo -n "-----> Configuring Datomic to connect to DEV... "
 
-    SAMPLE_PROPERTIES_FILE=${BUILD_DIR}/datomic/config/samples/sql-transactor-template.properties
+    SAMPLE_PROPERTIES_FILE=${BUILD_DIR}/datomic/config/samples/dev-transactor-template.properties
 
-    case ${STORAGE_TYPE} in
-
-        POSTGRES)
-            cat ${SAMPLE_PROPERTIES_FILE} |
-            license_filter | postgres_filter > ${OUTPUT_PROPERTIES_FILE}
-            ;;
-
-        HEROKU_POSTGRES)
-            cat ${SAMPLE_PROPERTIES_FILE} |
-            license_filter | postgres_filter | heroku_postgres_filter > ${OUTPUT_PROPERTIES_FILE}
-            ;;
-
-        *)  echo "Unsupported DB '${DATOMIC_STORAGE_TYPE}'" && return 1
-            ;;
-    esac
-
+    cat ${SAMPLE_PROPERTIES_FILE} | configure_properties > ${OUTPUT_PROPERTIES_FILE}
+    
     echo "done"
 }
 
-# Notes:
-# sed patterns use '|' as delimiters since '/' is found in URLs and license keys
-postgres_filter() {
-    SQL_URL=`echo ${DATABASE_URL} | sed 's?.*@\(.*\)?jdbc:postgresql://\1?'`
-    SQL_USER=`echo ${DATABASE_URL} | sed -e 's?postgres://\(.*\):.*?\1?' -e 's?\(.*\):.*?\1?'`
-    SQL_PASSWORD=`echo ${DATABASE_URL} | sed -e 's?postgres://\(.*\):.*?\1?' -e 's?.*:\(.*\)@.*?\1?'`
-
-    sed -e "s|^sql-url=.*|sql-url=${SQL_URL}|"                \
-        -e "s|^sql-user=.*|sql-user=${SQL_USER}|"             \
-        -e "s|^sql-password=.*|sql-password=${SQL_PASSWORD}|"
-}
-
-heroku_postgres_filter() {
-    SQL_DRIVER_PARAMS='ssl=true;sslfactory=org.postgresql.ssl.NonValidatingFactory'
-
-    sed "s|.*\(sql-driver-params=\).*|\1${SQL_DRIVER_PARAMS}|"
-}
-
-
-license_filter() {
-    sed "s|^license-key=.*|license-key=${DATOMIC_TRANSACTOR_KEY}|"
+configure_properties() {
+    sed -e "s|^pid-file=.*|pid-file=transactor.pid|"                                  \
+        -e "s|^storage-admin-password=.*|storage-admin-password=admin|"         \
+        -e "s|^storage-datomic-password=.*|storage-datomic-password=client|" \
+        -e "s|^storage-access=.*|storage-access=remote|" \
+        -e "s|^h2-port=.*|h2-port=4335|" \
+        -e "s|^license-key=.*|license-key=${DATOMIC_TRANSACTOR_KEY}|"
+    
 }
